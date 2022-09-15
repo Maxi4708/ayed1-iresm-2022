e8e0b19b59dbb8ae6416e389838e0089f7e7505b
Maxi4708 <maximilianomartinez4708@outlook.es>
2022-09-14 22:27:40 -0300

    first commit

---
+++ Clases Fede.py/main_4_6.py
@@ -0,0 +1,3 @@
+import funciones_4_6 as fu_4_6 #As sirve para acortar el nombre del codigo
+
+fu_4_6.menu() #Ponemos el punto para llamar a las funciones que querramos con el nombre del programa, junto con el ()
